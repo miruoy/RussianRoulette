b3e7505fc8da1287b797e66059781807466f12b1
Youri Matthys <2657774+miruoy@users.noreply.github.com>
2026-08-21 20:07:47 +0200

    RussianRoulette: import supybot toegevoegd in __init__.py

--- __init__.py
+++ __init__.py
@@ -14,6 +14,7 @@
 # with this program; if not, see <https://www.gnu.org/licenses/>.
 
 ### RussianRoulette — plugin entry point
+import supybot
 from . import plugin
 from . import config
 from importlib import reload
